e397e5d2e18bb90d10ca76c4b2fd694b01ddd0f6
zhaoleon <yuliang.zhao@lingyue-digital.com>
2024-07-25 08:28:16 +0200

    update script

--- Scripts/buildcookrun.sh
+++ Scripts/buildcookrun.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 runUatPath="/home/ubuntu/UE/Linux_Unreal_Engine_5.4.1/Engine/Build/BatchFiles/RunUAT.sh"
-uprojectPath="/home/ubuntu/actions-runner/_work/ActionsTest/ActionsTest/ActionsTest.uproject"
-buildConfig="Development"
-platform="linux"
-archive="true"
-archivePath="/home/ubuntu/ActionsOutput/"
-maps="false"
+buildConfig=$1 #"Development"
+platform=$2 #"linux"
+archive=$3 #"true"
+archivePath=${4:-"/home/ubuntu/ActionsWorkflowOutput/"}
+maps=$5 #"false"
 
 # Make sure there is only one uproject file in the project.
 uprojectCount=$(find .. -type f -name "*.uproject" | wc -l) 
@@ -19,11 +18,10 @@ fi
 uprojectPath=$(find .. -type f -name "*.uproject" | xargs -0 -I {} readlink -f {})
 
 archiveArg=$( [ "$archive" = 'true' ] && echo "-archive -archivedirectory=$archivePath" )
-encryptIniArg=$( [ "$encryptIni" = 'true' ] && echo "-encryptinifiles" )
-patchArg=$( [ "$patch" != 'false' ] && echo "-generatepatch -basedonreleaseversion=$patch" )
 mapsArg=$( [ "$maps" = 'true' ] && echo "" || echo "-map=$maps" )
 
 command="$runUatPath BuildCookRun -project=$uprojectPath -clientconfig=$buildConfig -platform=$platform $mapsArg -clean -cook -stage -pak -package $archiveArg -encryptinifiles -noP4 -build -unattended -utf8output"
 
 echo -e "buildcookrun commandline is:\n"$command
 sh -c "$command"
+
